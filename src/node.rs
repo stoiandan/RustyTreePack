7bfdd734ef5a1a41ac2cc6a18ddf7126f02e6ccf
Dan Stoian <danmihaistoian@gmail.com>
2021-09-25 10:43:49 +0300

    parent

--- src/node.rs
+++ src/node.rs
@@ -1,15 +1,14 @@
-use std::borrow::BorrowMut;
 use std::rc::{Rc, Weak};
 use std::cell::{RefCell};
 
 type NodeRef<T> = Option<Rc<RefCell<Node<T>>>>;
-type WeakNodeRef<T> = Weak<Node<T>>;
+type WeakNodeRef<T> = Weak<RefCell<Node<T>>>;
 
 #[derive(Debug)]
 pub struct Node<T> where T : PartialOrd {
-    pub left   : NodeRef<T>,
-    pub right  : NodeRef<T>,
-    pub parent : WeakNodeRef<T>,
+     left   : NodeRef<T>,
+     right  : NodeRef<T>,
+     parent : WeakNodeRef<T>,
     pub value  : T
 }
 
@@ -23,18 +22,22 @@ impl<T> Node<T> where T : PartialOrd {
 
     
      fn left_insert(&mut self, val : T) {
-         if let Some(left) = self.left  {
+         if let Some(left) = self.left.as_mut()  {
              left.borrow_mut().insert(val);
          } else {
-             self.left = Some(Rc::new(RefCell::new(Node::new(val))));
-         }
+             let rc = &Rc::new(RefCell::new(Node::new(val)));
+             self.left = Some(rc.clone());
+             self.left.as_ref().unwrap().borrow_mut().parent  = Rc::downgrade(&rc.clone());
+            }
     }
 
     fn right_insert(&mut self, val : T) {
         if let Some(right) = self.right.as_mut()  {
-            right.get_mut().insert(val);
+            right.borrow_mut().insert(val);
         } else {
-            self.right = Some(Rc::new(RefCell::new(Node::new(val))));
+            let rc = &Rc::new(RefCell::new(Node::new(val)));
+            self.right = Some(rc.clone());
+            self.right.as_ref().unwrap().borrow_mut().parent  = Rc::downgrade(&rc.clone());
         }
     }
 
@@ -51,10 +54,26 @@ impl<T> Node<T> where T : PartialOrd {
 #[cfg(test)]
 mod tree_test {
     use super::{Node};
+    use std::ptr;
     #[test]
     fn insert_right() {
-        let node = Node::new(6);
+        let mut node = Node::new(6);
         node.insert(8);
-        assert_eq!(node.right.unwrap().value, 8);
+        assert_eq!(node.right.as_mut().unwrap().borrow_mut().value, 8);
+    }
+
+    #[test]
+    fn insert_left() {
+        let mut node = Node::new(6);
+        node.insert(3);
+        assert_eq!(node.left.as_mut().unwrap().borrow_mut().value, 3);
+    }
+
+    #[test]
+    fn test_parent() {
+        let mut node = Node::new(5);
+        node.insert(6);
+        print!("{:p}\n{:p}",&node,&*node.right.as_ref().unwrap().borrow().parent.upgrade().as_ref().unwrap().borrow());
+        assert!(ptr::eq(&node,&*node.right.as_ref().unwrap().borrow().parent.upgrade().as_ref().unwrap().borrow()));
     }
 }
