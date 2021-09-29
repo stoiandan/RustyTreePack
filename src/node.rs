3f845a8d345a1961e02d15065c03bb540fd87746
Dan Stoian <danmihaistoian@gmail.com>
2021-09-29 11:43:59 +0300

    refactor

--- src/node.rs
+++ src/node.rs
@@ -44,78 +44,75 @@ where
         Tree { nodes: Vec::new() }
     }
 
-
     pub fn insert(&mut self, val: T) {
         self.insert_at(0, val);
     }
 
-    fn insert_at(&mut self, idx: usize, val: T)  {
-            if self.nodes.len() <= idx {
-                if idx == 0 {
-                    self.nodes.push(Node::new(val));
-                }
-                return;
+    fn insert_at(&mut self, idx: usize, val: T) {
+        if self.nodes.len() <= idx {
+            if idx == 0 {
+                self.nodes.push(Node::new(val));
             }
+            return;
+        }
 
-            let len = self.nodes.len();
-            let root = &mut self.nodes[idx];
+        let len = self.nodes.len();
+        let root = &mut self.nodes[idx];
 
-            // right insert
-            if val >= root.value {
-                if let Some(right) = root.right {
-                    self.insert_at(right, val);
-                } else {
-                    let mut new_node = Node::new(val);
-
-                    new_node.parent = Some(idx);
-                    root.right = Some(len);
-                    self.nodes.push(new_node);
-                }
+        // right insert
+        if val >= root.value {
+            if let Some(right) = root.right {
+                self.insert_at(right, val);
             } else {
-                if let Some(left) = root.left {
-                    self.insert_at(left, val);
-                } else {
-                    let mut new_node = Node::new(val);
+                let mut new_node = Node::new(val);
 
-                    new_node.parent = Some(idx);
-                    root.left = Some(len);
-                    self.nodes.push(new_node);
-                }
+                new_node.parent = Some(idx);
+                root.right = Some(len);
+                self.nodes.push(new_node);
             }
-    }
+        } else {
+            if let Some(left) = root.left {
+                self.insert_at(left, val);
+            } else {
+                let mut new_node = Node::new(val);
 
+                new_node.parent = Some(idx);
+                root.left = Some(len);
+                self.nodes.push(new_node);
+            }
+        }
+    }
 
     pub fn find(&self, val: T) -> Option<&Node<T>> {
         self.find_at(0, val)
     }
 
     fn find_at(&self, idx: usize, val: T) -> Option<&Node<T>> {
-                if self.nodes.len() <= idx {
-                    return None;
-                }
+        if self.nodes.len() <= idx {
+            return None;
+        }
 
-                let root = &self.nodes[idx];
+        let root = &self.nodes[idx];
 
-                if root.value == val {
-                    return Some(root);
-                }
+        if root.value == val {
+            return Some(root);
+        }
 
-                if root.value >= val {
-                    if let Some(left) = root.left {
-                        self.find_at(left, val)
-                    } else {
-                        return None
-                    }
-                } else {
-                    if let Some(right) = root.right {
-                        self.find_at(right, val)
-                    } else {
-                        return None
-                    }
-                }
+        if root.value >= val {
+            if let Some(left) = root.left {
+                self.find_at(left, val)
+            } else {
+                return None;
+            }
+        } else {
+            if let Some(right) = root.right {
+                self.find_at(right, val)
+            } else {
+                return None;
+            }
+        }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -136,7 +133,7 @@ mod tests {
         tree.insert(5);
         tree.insert(4);
 
-       assert_eq!(tree.find(4).unwrap().value, 4);
+        assert_eq!(tree.find(4).unwrap().value, 4);
     }
 
     #[test]
@@ -146,7 +143,7 @@ mod tests {
         tree.insert(5);
         tree.insert(7);
 
-        assert_eq!(tree.find(7).unwrap().value,7);
+        assert_eq!(tree.find(7).unwrap().value, 7);
     }
 
     #[test]
@@ -157,7 +154,7 @@ mod tests {
         tree.insert(7);
         tree.insert(3);
 
-       assert_eq!(tree.find(3).unwrap().value,3);
+        assert_eq!(tree.find(3).unwrap().value, 3);
     }
 
     #[test]
@@ -168,6 +165,6 @@ mod tests {
         tree.insert(5);
         tree.insert(3);
 
-       assert_eq!(tree.find(3).unwrap().value,3);
+        assert_eq!(tree.find(3).unwrap().value, 3);
     }
 }
